400adfc320bf25ef4d01378cb25c974819b796b9
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-09-19 17:00:31 -0500

    update testing

--- exercise.js
+++ exercise.js
@@ -8,18 +8,22 @@
 
 */
 
-
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const youShallNotPass = (arr) => {
-    /* Only make changes below this comment */
-    
-    
-    /* Only make changes below this comment */
-}
+  /* Only make changes below this comment */
 
-
-
-
+  if (!arr) {
+    return console.warn("Please input a array");
+  } else {
+    var arrFilter = arr.filter(Boolean);
+    return arrFilter;
+  }
+  /* Only make changes below this comment */
+};
+console.log(youShallNotPass([]));
+console.log(youShallNotPass([false, null, "", 0])); //[]
+console.log(youShallNotPass(["hello world!", NaN, undefined, 1, 2])); //['hello world!', 1, 2]
+console.log(youShallNotPass([10, false, "", "pass"])); //[10, 'pass']
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.youShallNotPass = youShallNotPass;
