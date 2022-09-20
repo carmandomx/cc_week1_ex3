f3d60ad81a7a3dd9064c0787e4d37a3eb0416857
Fredy Guzman <fredy.guzman@itksquare.edu.mx>
2022-09-19 21:28:39 -0500

    Answered

--- exercise.js
+++ exercise.js
@@ -1,6 +1,6 @@
 /* *Instructions*
     
-    There's somethign called falsy values in JavaScript and this exercise will help you in learning them.
+    There's somethign called git  in JavaScript and this exercise will help you in learning them.
     
     You will receive an array with all kinds of values and you need to remove all falsy values from the given array and
     return the array with the remaining values.
@@ -12,7 +12,26 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const youShallNotPass = (arr) => {
     /* Only make changes below this comment */
+    let remainingValues=[];
+    let arrSize = arr.length;
+    let remainingValue;
+    console.log(arrSize);
+    if(arr.length==0){
+        remainingValues=arr.slice();
+    }else{
+        for(let i=0;i<arrSize;i++){
+
+            
+            if(Boolean(arr[i])){
+                remainingValues.push(arr[i]);
+            }
+        }
+        
+    }
     
+    console.log("hola "+ remainingValues);
+    return remainingValues;
+
     
     /* Only make changes below this comment */
 }
