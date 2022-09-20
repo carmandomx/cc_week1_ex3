b2bc9d3cc1bafc329e1bc08980d92c0fe17133e2
Yumil Rueda <Yumil.Flores@itksquare.edu.mx>
2022-09-20 08:27:30 +0200

    DONE Yumil

--- exercise.js
+++ exercise.js
@@ -12,7 +12,26 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const youShallNotPass = (arr) => {
     /* Only make changes below this comment */
+
+    //Verification of lenght
+    console.log(arr.length/2);
+
+    //variable for the new array
+    let arr_result = [];
+    //Cycle to check all the elements in the arrray.
     
+    for(let i = 0;i<arr.length;i++){
+        if (arr[i]) {
+            //if the element it is NOT a falsy, add the element to my new array (the answer) 
+            arr_result.push(arr[i]);
+          }
+    }
+
+    //Show in the console to confirme my result
+    //console.log(arr_result)
+
+    //return of my array
+    return arr_result;
     
     /* Only make changes below this comment */
 }
