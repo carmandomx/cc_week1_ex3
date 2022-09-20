36fdd153ce0ea427d822e086e0cac47522324d4d
MarianaLopez-Ksquare <Mariana.Lopez@itksquare.edu.mx>
2022-09-20 14:18:25 -0500

    First Commit

--- exercise.js
+++ exercise.js
@@ -8,18 +8,18 @@
 
 */
 
-
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const youShallNotPass = (arr) => {
+    
     /* Only make changes below this comment */
-    
-    
+
+    /* filter () creates a new array with all the elements */
+    /*that meet the condition implemented by the given function.*/
+
+    const NewArray = arr.filter(Boolean);
+    return NewArray;
+        
     /* Only make changes below this comment */
 }
-
-
-
-
-
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.youShallNotPass = youShallNotPass;
