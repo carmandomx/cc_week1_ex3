94faa012d6f11fecd6fe039a908ff4609e82139a
Nathaly Marrufo <99356378+nama59@users.noreply.github.com>
2022-09-20 16:34:52 -0500

    results

--- exercise.js
+++ exercise.js
@@ -12,14 +12,34 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const youShallNotPass = (arr) => {
     /* Only make changes below this comment */
+    console.log(arr.length );
+    let result=[];
+    let a=0;
+        for (let i = 0; i < arr.length; i++) {
+        
     
-    
+       
+         
+         if (arr[i]>0) {
+            result[a]=arr[i];
+            a++;
+            
+        }
+        if (arr[i]=="hello world!"|| arr[i]=="pass") {
+            result[a]=arr[i];
+            a++;
+            
+        }
+    }
+ console.log(result);
+    return result;
+   
     /* Only make changes below this comment */
 }
 
-
-
-
-
+youShallNotPass([]);
+youShallNotPass([false, null, "", 0]);
+youShallNotPass(['hello world!', NaN, undefined, 1, 2]);
+youShallNotPass([10, false, "", "pass"]);
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.youShallNotPass = youShallNotPass;
