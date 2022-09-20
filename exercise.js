b72458c2fe1ea852fe334c3b882167b62f698a4d
“Dannaé <claudia.diaz@itksquare.edu.mx>
2022-09-20 15:26:49 -0500

    Redoing the exercise using a FOR loop

--- exercise.js
+++ exercise.js
@@ -13,12 +13,30 @@
 const youShallNotPass = (arr) => {
     /* Only make changes below this comment */
 
-    // Declaring an array that will store truthy values by filtering (gets rid of falsy values)
-    const truthyArr = arr.filter(function(value) { 
-        return value; // only returns truthy values, equivalent to using boolean
-    });
+    // ==================== Using the suggested "for loop" =========================
+    let truthyArr = []; // initially empty, will be filled with truthy values
+    for (let i = 0; i < arr.length; i++) {
+        let value = arr[i]; // loop through each value of the given array 
+        
+        if (value) {
+            // It'll enter the "if" only when the value is truthy
+            // in that case: add the value to the truthy array
+            truthyArr.push(value);
+        } else {
+            // otherwise it will skip pushing that value into our truthy array
+            continue;
+        }
+    }
+    return truthyArr; // return the new array after looping
 
-    return truthyArr;
+    // // ==================== A much simpler version using "filter" =========================
+    // // Declare an array that will store truthy values by filtering (gets rid of falsy values)
+    // const truthyArr = arr.filter(function(value) { 
+    //     return value; // only returns truthy values, equivalent to using boolean
+    // });
+
+    // return truthyArr;
+
     /* Only make changes below this comment */
 }
 
