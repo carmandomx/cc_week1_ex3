3422419f021157859ba684b508320b0c2350a41d
EstrellaPoot <Estrella.Poot@itksquare.edu.mx>
2022-09-20 16:21:20 -0500

    Complete function to remove falsy values in an array

--- exercise.js
+++ exercise.js
@@ -12,8 +12,20 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const youShallNotPass = (arr) => {
     /* Only make changes below this comment */
-    
-    
+        // Initialize the variable arrTruthy as an empty array
+        let arrTruthy = [];
+        // Evaluate with a foreach the received array
+        arr.forEach(element => {
+            /* 
+                if the element evaluated is truthy then it is added to the arrTruthy array 
+            */
+            if(element){
+                arrTruthy.push(element);
+            }
+        });
+        // array without falsy values is returned
+        return arrTruthy;
+
     /* Only make changes below this comment */
 }
 
