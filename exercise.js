ecbf708d92580ed9057c27b6169690fdb5699011
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-09-20 11:17:05 -0500

    update commits and notes

--- exercise.js
+++ exercise.js
@@ -12,14 +12,17 @@
 const youShallNotPass = (arr) => {
   /* Only make changes below this comment */
 
-  if (!arr) {
+  //first evaluation, if "arr" not exist we send a warning or a second evaluation, for cases when arr is not a Array element we return a warning.
+  if (!arr || !Array.isArray(arr)) {
     return console.warn("Please input a array");
   } else {
+    //if all it's ok, we filter the array deleting all boolean values and return them in a new value "arrFilter"
     var arrFilter = arr.filter(Boolean);
     return arrFilter;
   }
   /* Only make changes below this comment */
 };
+
 console.log(youShallNotPass([]));
 console.log(youShallNotPass([false, null, "", 0])); //[]
 console.log(youShallNotPass(["hello world!", NaN, undefined, 1, 2])); //['hello world!', 1, 2]
