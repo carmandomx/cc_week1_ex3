ba857e1af9bb963fcafbc048ece27fc606d1df2c
AlonsoDelaCruz-Ksquare <113466701+AlonsoDlaCruz@users.noreply.github.com>
2022-09-20 17:21:11 -0500

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -12,7 +12,37 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const youShallNotPass = (arr) => {
     /* Only make changes below this comment */
-    
+    var workarray=[]; //Empty array to store the truly values
+    var j=0; //Variable to count the good ones
+    var arrvalue; //undefined variable to temp store values
+
+    for (let i = 0; i < arr.length; i++) //checking all the values
+    {
+
+        //Comparing if it is falsy
+        arrvalue=arr[i];
+
+
+        if(arrvalue > "" && arrvalue!==undefined) //not empty neither undefined
+        {
+            if(arrvalue !== null
+                && arrvalue !== NaN && arrvalue != 0 && arrvalue !==false)
+                {
+
+                    workarray[j]=arrvalue;
+                    j++;
+
+                }
+                
+                
+        } 
+
+        
+            
+        
+
+    }
+    return workarray;
     
     /* Only make changes below this comment */
 }
