22efb7084fbd585b09790031f44cb405a592386b
Brandon Hernandez <Brandon.Hernandez@itksquare.edu.mx>
2022-09-20 15:23:22 -0500

    final test w/comments

--- exercise.js
+++ exercise.js
@@ -12,11 +12,11 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const youShallNotPass = (arr) => {
     /* Only make changes below this comment */
-    
-    
+    let newArr = arr.filter(Boolean); //we create a new array that will store only truthy values using the boolean constructor (boolean cosntructor will only return truthy values)
+    return newArr; //return new array filtered with only truthy values 
     /* Only make changes below this comment */
 }
-
+console.log(youShallNotPass([NaN, 76, "gfg", true, 12, "hi", [], false])); //function with example array to test correct use of function
 
 
 
