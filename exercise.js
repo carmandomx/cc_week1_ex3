7f6152855e4886e72bcbdbcafb53b01d56624933
Nathaly Marrufo <99356378+nama59@users.noreply.github.com>
2022-09-20 16:44:30 -0500

    final

--- exercise.js
+++ exercise.js
@@ -12,34 +12,21 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const youShallNotPass = (arr) => {
     /* Only make changes below this comment */
-    console.log(arr.length );
-    let result=[];
-    let a=0;
-        for (let i = 0; i < arr.length; i++) {
-        
     
-       
-         
-         if (arr[i]>0) {
-            result[a]=arr[i];
-            a++;
-            
-        }
-        if (arr[i]=="hello world!"|| arr[i]=="pass") {
-            result[a]=arr[i];
-            a++;
-            
-        }
+    let result=[]; //Here will be store the result for the test
+
+
+        for (let i = 0; i < arr.length; i++) { //This is going to pass through the whole array
+            if (arr[i]) { //here we are going to evaluate the array one by one and if it has a value is going to follow instructions
+                result.push(arr[i]); //if the condition is true, the function push is going to take the value inside of the result array.
+            }
     }
- console.log(result);
+
+ 
     return result;
    
     /* Only make changes below this comment */
 }
 
-youShallNotPass([]);
-youShallNotPass([false, null, "", 0]);
-youShallNotPass(['hello world!', NaN, undefined, 1, 2]);
-youShallNotPass([10, false, "", "pass"]);
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.youShallNotPass = youShallNotPass;
