77e76d76fa9a75b5ec79200eb1df60b7c11088ca
Alan Villanueva <alan.villanueva@itksquare.edu.mx>
2023-03-01 21:09:57 -0600

    solved

--- exercise.js
+++ exercise.js
@@ -12,12 +12,18 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const youShallNotPass = (arr) => {
     /* Only make changes below this comment */
-    
-    
+    let len_arr=arr.length
+    let non_falsey=[]
+    for (let i=0;i<len_arr;i++){
+        if (arr[i]){
+            non_falsey.push(arr[i])
+        }
+    }
+    return non_falsey
     /* Only make changes below this comment */
 }
 
-
+/*youShallNotPass(['ALAN',23,0,'',[1,2],3,null])/
 
 
 
